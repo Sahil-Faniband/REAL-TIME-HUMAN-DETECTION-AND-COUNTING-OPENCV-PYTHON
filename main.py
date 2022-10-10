18e0f9b6671c1dc3bd6e3063303b9ccd85bd248f
Sahil Faniband <112681078+INFINITY-962@users.noreply.github.com>
2022-10-10 10:24:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,113 @@
+import cv2 # library of Python bindings designed to solve computer vision problems. cv2
+import imutils #basic image processing
+import numpy as np
+import argparse #helps create a program in a command-line-environment
+#frame
+def detect(frame):
+    bounding_box_cordinates, weights = HOGCV.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=0.5)
+    #detectMultiScale function is used to detect the faces.
+    # This function will return a rectangle with coordinates(x,y,w,h) around the detected face.
+    person = 1
+    for x, y, w, h in bounding_box_cordinates:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2) # used to draw a rectangle on  image.
+        cv2.putText(frame, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)# used to draw a text string on detected image
+        person += 1
+    
+    cv2.putText(frame, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(frame, f'Total Persons : {person-1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.imshow('output', frame)# display an image in a window
+
+    return frame
+
+def detectByPathVideo(path, writer):
+    video = cv2.VideoCapture(path) # This will return video from the video to which path is funtion parameter  on your computer.
+    check, frame = video.read()
+    if check == False:
+        print('Video Not Found. Please Enter a Valid Path (Full path of Video Should be Provided).')
+        return
+
+    print('Detecting people...')
+    while video.isOpened():
+        #check is True if reading was successful 
+        check, frame = video.read()
+
+        if check:
+            frame = imutils.resize(frame, width=min(800, frame.shape[1]))
+            frame = detect(frame)
+            
+            if writer is not None:
+                writer.write(frame)
+            
+            key = cv2.waitKey(1) #allows display a window for given milliseconds
+            if key == ord('q'):
+                break
+        else:
+            break
+    video.release()
+    cv2.destroyAllWindows()
+
+def detectByCamera(writer):
+    video = cv2.VideoCapture(0) # This will return video from the first webcam on your computer.
+    print('Detecting people...')
+    while True:
+        check, frame = video.read()
+
+        frame = detect(frame)
+        if writer is not None:
+            writer.write(frame)
+
+        key = cv2.waitKey(1) #allows display a window for given milliseconds
+        if key == ord('q'):
+            break
+
+    video.release()
+    cv2.destroyAllWindows()
+
+def detectByPathImage(path, output_path):
+    image = cv2.imread(path)
+    image = imutils.resize(image, width=min(800, image.shape[1]))
+    result_image = detect(image)
+    if output_path is not None:
+        cv2.imwrite(output_path, result_image) #This will save the image according to the specified format in current working directory.
+    cv2.waitKey(0) #It takes time in milliseconds as a parameter and waits for the given time to destroy the window,
+                   # if 0 is passed in the argument it waits till any key is pressed
+    cv2.destroyAllWindows()
+
+
+def humanDetector(args):
+    image_path = args["image"]
+    video_path = args['video']
+    if str(args["camera"]) == 'true': camera = True
+    else: camera = False
+
+    writer = None
+    if args['output'] is not None and image_path is None:
+        writer = cv2.VideoWriter(args['output'], cv2.VideoWriter_fourcc(*'MJPG'), 10, (600, 600))
+
+    if camera:
+        print('[INFO] Opening Web Cam.')
+        detectByCamera(writer)
+    elif video_path is not None:
+        print('[INFO] Opening Video from path.')
+        detectByPathVideo(video_path, writer)
+    elif image_path is not None:
+        print('[INFO] Opening Image from path.')
+        detectByPathImage(image_path, args['output'])
+
+def argsParser():
+    arg_parse = argparse.ArgumentParser() # intialize the parser start to add custm arguments
+    arg_parse.add_argument("-v", "--video", default=None, help="path to Video File ")# command
+    arg_parse.add_argument("-i", "--image", default=None, help="path to Image File ")# command
+    arg_parse.add_argument("-c", "--camera", default=False, help="Set true if you want to use the camera.")# command
+    arg_parse.add_argument("-o", "--output", type=str, help="path to optional output video file")# command
+    args = vars(arg_parse.parse_args()) # get an object's _dict_ attribute
+
+    return args
+
+if __name__ == "__main__":
+    HOGCV= cv2.HOGDescriptor() #( Histograms of Oriented Gradients)object detector.
+    HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())#set the trainer classifiers
+    args = argsParser()#recommended command-line parsing module
+    humanDetector(args)
+
+#main module of the system
